957fa9c6972f2c99b902194c586667663975183c
Łukasz Piestrzeniewicz <bragi@ragnarson.com>
2013-03-17 17:41:18 +0100

    Added simple error handling

--- passage.rb
+++ passage.rb
@@ -20,14 +20,19 @@ class Passage
     Nokogiri::HTML(content)
   end
   
+  def has_verses?
+    !verses.empty?
+  end
+  
   def verses
+    return @v if @v
     v = doc.css("span.werset").xpath("text()").map do |verse| 
       verse.to_s.encode(Encoding::UTF_8).strip
     end.reject do |verse| 
       verse == ""
     end
     first = book.first - 1
-    last = book.last || v.length
-    v[first..last]
+    last = book.last ? (book.last - 1) : v.length
+    @v = v[first..last] || []
   end
 end
