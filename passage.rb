560e331234cc46310166e1d22deaa35a482884b5
Łukasz Piestrzeniewicz <bragi@ragnarson.com>
2013-03-23 19:37:18 +0100

    Passage reads text from local filesystem, #3

--- passage.rb
+++ passage.rb
@@ -1,23 +1,12 @@
-require 'open-uri'
-require 'nokogiri'
-
 class Passage
   attr_reader :book
   
   def initialize(book)
     @book = book
   end
-  
-  def url
-    "http://www.biblia.info.pl/cgi-bin/biblia-nawigator.cgi?tlumaczenie=bw&rozdzial=#{@book.chapter}&st=i&ks=#{@book.short_name}&nw=tak"
-  end
-  
-  def content
-    open(url).read
-  end
-  
-  def doc
-    Nokogiri::HTML(content)
+
+  def file
+    File.expand_path("data/#{@book.short_name}#{@book.chapter}.txt", File.dirname(__FILE__))
   end
   
   def has_verses?
@@ -25,14 +14,10 @@ class Passage
   end
   
   def verses
-    return @v if @v
-    v = doc.css("span.werset").xpath("text()").map do |verse| 
-      verse.to_s.encode(Encoding::UTF_8).strip
-    end.reject do |verse| 
-      verse == ""
-    end
+    return @verses if @verses
+    verses = File.readlines(file)
     first = book.first - 1
-    last = book.last ? (book.last - 1) : v.length
-    @v = v[first..last] || []
+    last = book.last ? (book.last - 1) : verses.length
+    @verses = verses[first..last] || []
   end
 end
