8b01e80729aa1a7254ca2174994d648f207a4b0c
Łukasz Piestrzeniewicz <bragi@ragnarson.com>
2013-05-26 12:23:00 +0200

    #7 added tests for addresses with multiple single verses

--- test/address_string_test.rb
+++ test/address_string_test.rb
@@ -11,7 +11,7 @@ class AddressStringTest < MiniTest::Unit::TestCase
   def test_whole_chapter
     assert_equal ["Gen", "2", "1-"], AddressString.new("Gen2").to_params
   end
-  
+
   def test_whole_chapter_with_numeric_name
     assert_equal ["1Mch", "2", "1-"], AddressString.new("1Mch2").to_params
   end
@@ -19,15 +19,19 @@ class AddressStringTest < MiniTest::Unit::TestCase
   def test_with_start_and_no_end
     assert_equal ["Gen", "2", "3-"], AddressString.new("Gen2:3-").to_params
   end
-  
+
   def test_with_start_and_end
     assert_equal ["Gen", "2", "3-7"], AddressString.new("Gen2:3-7").to_params
+  end
+
+  def test_with_single_verses
+    assert_equal ["Gen", "2", "3-7,8,9-10"], AddressString.new("Gen2:3-7,8,9-10").to_params
   end
 
   def test_whole_chapter_with_space
     assert_equal ["Gen", "2", "1-"], AddressString.new("Gen 2").to_params
   end
-  
+
   def test_whole_chapter_with_numeric_name_with_space
     assert_equal ["1Mch", "2", "1-"], AddressString.new("1Mch 2").to_params
   end
@@ -35,7 +39,11 @@ class AddressStringTest < MiniTest::Unit::TestCase
   def test_with_start_and_no_end_with_space
     assert_equal ["Gen", "2", "3-"], AddressString.new("Gen 2:3-").to_params
   end
-  
+
+  def test_with_start_and_end_with_space
+    assert_equal ["Gen", "2", "3-7"], AddressString.new("Gen 2:3-7").to_params
+  end
+
   def test_with_start_and_end_with_space
     assert_equal ["Gen", "2", "3-7"], AddressString.new("Gen 2:3-7").to_params
   end
