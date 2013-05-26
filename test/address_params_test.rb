d4b368a2d0f4295bbe4b2a41463ea2d20871d36b
Łukasz Piestrzeniewicz <bragi@ragnarson.com>
2013-05-26 12:19:36 +0200

    #7 added tests for converting multiple verses to address

--- test/address_params_test.rb
+++ test/address_params_test.rb
@@ -15,17 +15,25 @@ class AddressParamsTest < MiniTest::Unit::TestCase
   def test_whole_chapter
     assert_equal "Gen2", AddressParams.new(book: "Gen", chapter: "2").to_s
   end
-  
+
   def test_with_start_and_no_end
     assert_equal "Gen2:3-", AddressParams.new(book: "Gen", chapter: "2", verses: "3-").to_s
   end
-  
+
   def test_with_start_and_end
     assert_equal "Gen2:3-7", AddressParams.new(book: "Gen", chapter: "2", verses: "3-7").to_s
   end
 
   def test_single_verse
     assert_equal "Gen2:3", AddressParams.new(book: "Gen", chapter: "2", verses: "3").to_s
+  end
+
+  def test_multiple_separate_verses
+    assert_equal "Gen2:3,7,8", AddressParams.new(book: "Gen", chapter: "2", verses: "3,7,8").to_s
+  end
+
+  def test_multiple_verse_ranges
+    assert_equal "Gen2:3,7-8,10", AddressParams.new(book: "Gen", chapter: "2", verses: "3,7-8,10").to_s
   end
 
   def test_without_chapter_ignore_verses
